193c451e5cd92f5c09fc0ad7f6bef1107185c004
SaifSamir1 <saifsamirsaif12@gmail.com>
2024-07-14 17:57:58 +0300

    test commit in dev branch

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_advanced_app/core/routing/app_router.dart';
 import 'package:flutter_advanced_app/doc_app.dart';
 
 void main() {
+
   runApp( DocApp(appRouter: AppRouter(),));
 }
 
